a0dc9c604d427e2d2854612df0e9787b4fc4b0d2
Eugene Abramchuk <yevhen.abramchuk@scylladb.com>
2026-01-05 21:20:06 +0100

    debug

--- sdcm/utils/features.py
+++ sdcm/utils/features.py
@@ -88,7 +88,7 @@ def is_tablets_feature_enabled(node: BaseNode) -> bool:
     with node.remote_scylla_yaml() as scylla_yaml:
         # for backward compatibility of 2024.1 and earlier
         scylla_conf = scylla_yaml.model_dump()
-        LOGGER.debug("##### Scylla config: %s", scylla_conf)
+        raise ValueError(f"##### Scylla config: {scylla_conf}")
         if "tablets" in (scylla_conf.get("experimental_features") or []):
             return True
         if scylla_conf.get("enable_tablets"):
